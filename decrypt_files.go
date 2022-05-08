74a29402457aad7d42f0b463c6dc8b614175fef1
can-er <2broglie@gmail.com>
2022-05-08 23:15:36 +0200

    fixing bugs

--- decrypt_files.go
+++ decrypt_files.go
@@ -4,12 +4,9 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/md5"
-	"crypto/rand"
 	"encoding/hex"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"log"
 	"os"
 )
 
@@ -17,20 +14,6 @@ func createHash(key string) string {
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
-}
-
-func encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	return ciphertext
 }
 
 func decrypt(data []byte, passphrase string) []byte {
@@ -52,12 +35,6 @@ func decrypt(data []byte, passphrase string) []byte {
 	return plaintext
 }
 
-func encryptFile(filename string, data []byte, passphrase string) {
-	f, _ := os.Create(filename)
-	defer f.Close()
-	f.Write(encrypt(data, passphrase))
-}
-
 func decryptFile(filename string, passphrase string) []byte {
 	data, _ := ioutil.ReadFile(filename)
 	return decrypt(data, passphrase)
@@ -71,13 +48,8 @@ func main() {
 	//fmt.Printf("Decrypted: %s\n", plaintext)
 	//encryptFile("sample.txt", []byte("foo"), "password1")
 	//fmt.Println(string(decryptFile("sample.txt", "password1")))
-
 	files := os.Args[1:]
 	for i := 0; i < len(files); i++ {
-		content, err := os.ReadFile(files[i])
-		if err != nil {
-			log.Fatal(err)
-		}
 		fmt.Println(string(decryptFile(files[i], "password1")))
 	}
 
--- encrypt_files.go
+++ encrypt_files.go
@@ -7,7 +7,6 @@ import (
 	"crypto/rand"
 	"encoding/hex"
 	"io"
-	"io/ioutil"
 	"log"
 	"os"
 )
@@ -32,34 +31,10 @@ func encrypt(data []byte, passphrase string) []byte {
 	return ciphertext
 }
 
-func decrypt(data []byte, passphrase string) []byte {
-	key := []byte(createHash(passphrase))
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	return plaintext
-}
-
 func encryptFile(filename string, data []byte, passphrase string) {
 	f, _ := os.Create(filename)
 	defer f.Close()
 	f.Write(encrypt(data, passphrase))
-}
-
-func decryptFile(filename string, passphrase string) []byte {
-	data, _ := ioutil.ReadFile(filename)
-	return decrypt(data, passphrase)
 }
 
 func main() {
