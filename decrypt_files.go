c18e0da49e4c80828e220ab2b3f840ab97272203
can-er <2broglie@gmail.com>
2022-05-09 00:59:16 +0200

    commenting the code

--- decrypt_files.go
+++ decrypt_files.go
@@ -12,21 +12,25 @@ import (
 )
 
 func createHash(key string) string {
+	// Create a new hash object
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func decrypt(data []byte, passphrase string) []byte {
+	// Create a new AES cipher block
 	key := []byte(createHash(passphrase))
 	block, err := aes.NewCipher(key)
 	if err != nil {
 		panic(err.Error())
 	}
+	// Create a new GCM cipher
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+	// Create a new nonce
 	nonceSize := gcm.NonceSize()
 	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
 	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
@@ -37,6 +41,7 @@ func decrypt(data []byte, passphrase string) []byte {
 }
 
 func decryptFile(filename string, passphrase string) []byte {
+	// Decrypt the file content
 	data, _ := ioutil.ReadFile(filename)
 	return decrypt(data, passphrase)
 }
@@ -51,12 +56,13 @@ func main() {
 	//fmt.Println(string(decryptFile("sample.txt", "password1")))
 
 	var dir = os.Args[1]
-
 	files, err := ioutil.ReadDir(dir)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Loop through the files to decrypt
 	for i := 0; i < len(files); i++ {
 		var filePath = dir + "/" + string(files[i].Name())
 		fmt.Println(string(decryptFile(filePath, "password1")))
--- encrypt_files.go
+++ encrypt_files.go
@@ -13,26 +13,31 @@ import (
 )
 
 func createHash(key string) string {
+	// Create a new hash object
 	hasher := md5.New()
 	hasher.Write([]byte(key))
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
 func encrypt(data []byte, passphrase string) []byte {
+	// Create a new AES cipher block
 	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
 	gcm, err := cipher.NewGCM(block)
 	if err != nil {
 		panic(err.Error())
 	}
+	// Create a new nonce
 	nonce := make([]byte, gcm.NonceSize())
 	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
 		panic(err.Error())
 	}
+	// Encrypt the data
 	ciphertext := gcm.Seal(nonce, nonce, data, nil)
 	return ciphertext
 }
 
 func encryptFile(filename string, data []byte, passphrase string) {
+	// Encrypt the file content
 	f, _ := os.Create(filename)
 	defer f.Close()
 	f.Write(encrypt(data, passphrase))
@@ -54,6 +59,7 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Iterate over the files to encrypt
 	for i := 0; i < len(files); i++ {
 		var filePath = dir + "/" + string(files[i].Name())
 		content, err := os.ReadFile(filePath)
